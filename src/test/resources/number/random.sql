952a892de45f4e64cf80e499ed2f1bccee22be84
binsong.li <binsong.li@tongdun.cn>
2021-07-12 13:34:42 +0800

    update

--- src/test/resources/number/random.sql
+++
@@ -1,4 +0,0 @@
-create database test_db;
-create table test_db.hello_world (name string, age string) partitioned by (day string) stored as orc;
-create temporary function randomInt as 'com.dataworker.udf.number.GenericUDFRandomInt';
-insert into test_db.hello_world partition(day='2017-05-19') (name, age) values ('zhangsan', '13');
