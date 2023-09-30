d48357dfa698d33acaba015d8c6006232bcdf055
melin <libinsong1204@gmail.com>
2023-09-30 15:24:44 +0800

    update

--- src/main/java/com/superior/udf/starrocks/HelloUDF.java
+++ src/main/java/com/superior/udf/starrocks/HelloUDF.java
@@ -2,9 +2,9 @@ package com.superior.udf.starrocks;
 
 public class HelloUDF {
 
-    public final String evaluate(String name) {
+    public final String evaluate(String name, Integer age) {
         if (name == null) return null;
 
-        return "Hello " + name;
+        return "Hello " + name + ", age " + age;
     }
 }
